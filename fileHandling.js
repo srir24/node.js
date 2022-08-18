4979dc8e20369b244c1f2d844494e93661211609
srir24 <ravula.srikanh@gmail.com>
2022-08-18 23:08:29 +0530

    Delete and read file/folder code updated

--- fileHandling.js
+++ fileHandling.js
@@ -6,7 +6,50 @@ h1.writeFile('sample.js','console.log("Hello")',
 h1.appendFile('sample.js','console.log("Hello 123")',
                             function(err){console.log("Hi in append")})
 
-h1.readFile('sample.js','utf-8',function(err,data){console.log(  data)})
 
-h1.unlink('sample.js',function(err){console.log("Hi i am in delete")})
+//Reading file Option-1
+h1.readFile('sample.js','utf-8',function(err,data){console.log(data)})
+
+//Reading file Option-2
+h1.readFile('sample.js',function(err,data){
+  if(err){
+    console.log(err)
+  }
+  console.log(data)
+})
+
+//Reading file Option-2
+h1.readFile('sample.js',(err,data)=>{
+  if(err){
+    console.log(err)
+  }
+  console.log(data)
+})
+
+
+//Folder/Directory creation and deletion
+if(!h1.existsSync("./test"))
+  h1.mkdir("./test",(err)=>{
+  if(err){ 
+console.log(err)
+  }
+  console.log("Folder created")
+})
+else{
+  h1.rmdir("./test",(err)=>{
+    if(err){
+      console.log(err)
+    }
+  })
+  console.log("Folder removed")
+}
+
+//Removing/Deleting the file
+if(h1.existsSync("./sample.js")){
+    h1.unlink('sample.js',function(err){
+    if(err){
+      console.log(err)
+    }
+    console.log("File removed")
+  })
 
